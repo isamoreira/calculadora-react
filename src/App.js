1ae08366956b759934acfd2b6fa9e78375d5ff50
Isabella Moreira <babellamoreira@gmail.com>
2022-10-01 17:15:39 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import Header from "./components/Header.js";
 import Main from "./components/Main.js";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: linear-gradient(#e66465, #9198e5);
+  margin: -20px;
+  padding: 10px;
+`;
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <Header />
         <Main />
-      </div>
+      </Container>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,6 @@ const H1 = styled.h1`
   display: flex;
   justify-content: center;
   color: white;
-  background-color: gray;
-  border: solid 2px black;
-  border-radius: 20px;
 `;
 
 export default class Header extends Component {
